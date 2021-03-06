d03a344b9d7b8c0612b09c06c8f71c1e214f754a
Rafael Miranda <rafaelpadovezi@gmail.com>
2021-03-06 18:07:55 -0300

    chorus: fix files

---
+++ src/Consumers/IConsumer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BookmarkManager.Consumers
+{
+    public interface IConsumer<TMessage>
+    {
+        Task ExecuteAsync(TMessage message, Action ack);
+    }
+}
