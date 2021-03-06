1b4abb9fd9d0d43f6bac32cfdb7ecbff96fb9f75
Rafael Miranda <rafaelpadovezi@gmail.com>
2021-03-06 19:06:26 -0300

    feature: use cli lib to start command

--- src/Infrastructure/Queues/BookmarkInsertedQueue.cs
+++ src/Infrastructure/Queues/BookmarkInsertedQueue.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 
 namespace BookmarkManager.Infrastructure
 {
-
     public sealed class BookmarkInsertedQueue : IQueue<BookmarkInserted>
     {
         private const string _queueName = "bookmark.inserted";
