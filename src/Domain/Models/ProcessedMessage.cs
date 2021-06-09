867f584d77ca4fb0a1ec673dc9da32c8cc3c87fb
Rafael Miranda <rafaelpadovezi@gmail.com>
2021-06-08 22:21:09 -0300

    feature: track processed messages

---
+++ src/Domain/Models/ProcessedMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace BookmarkManager.Domain.Models
+{
+    public class ProcessedMessage
+    {
+        public Guid Id { get; set; }
+        public string ConsumerName { get; set; }
+
+        // Used by EF
+        private ProcessedMessage()
+        {
+        }
+
+        public ProcessedMessage(Guid id, Type consumerType)
+        {
+            Id = id;
+            ConsumerName = consumerType.FullName;
+        }
+
+        public Expression<Func<ProcessedMessage, bool>> IsEqual()
+        {
+            return x => x.Id == Id && x.ConsumerName == ConsumerName;
+        }
+    }
+}
